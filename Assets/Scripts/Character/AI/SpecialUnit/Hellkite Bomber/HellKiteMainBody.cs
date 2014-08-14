e43992669e7a3fccb374b091221e1299a9a7633c
kjjeon5204 <bboybyung@gmail.com>
2014-08-13 20:19:55 -0400

    Update

--- Assets/Scripts/Character/AI/SpecialUnit/Hellkite Bomber/HellKiteMainBody.cs
+++ Assets/Scripts/Character/AI/SpecialUnit/Hellkite Bomber/HellKiteMainBody.cs
@@ -6,6 +6,18 @@ public class HellKiteMainBody : Character {
     int spawnPhase = 0;
     Vector3 easingDirection;
 
+    public GameObject[] missilePodLW;
+    public GameObject[] missilePodLB;
+    public GameObject[] missilePodRW;
+    public GameObject[] missilePodRB;
+
+    public GameObject tailGunTurretControl;
+
+    bool missileControlsLW;
+    bool missileControlsLB;
+    bool missileControlsRB;
+    bool missileControlsRW;
+    
 
     public override void manual_start()
     {
@@ -19,14 +31,17 @@ public class HellKiteMainBody : Character {
 	// Update is called once per frame
 	public override void manual_update () {
         float distanceToTarget = (target.transform.position - transform.position).magnitude;
-
+        Vector3 playerRelativePos = transform.InverseTransformPoint(target.transform.position);
 
         custom_look_at_3D(target.transform.position);
         transform.Translate(3.0f * Vector3.forward * Time.deltaTime);
 
-
-	    foreach (HellKiteTurret turret in myTurrets) {
-            turret.update_turret();
+        if (playerRelativePos.y < 0.0f)
+        {
+            foreach (HellKiteTurret turret in myTurrets)
+            {
+                turret.update_turret();
+            }
         }
 	}
 
